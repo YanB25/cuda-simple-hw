49cb732354a6a617ceb686acc4660d4407ae7f69
yanbin <yanb25@mail2.sysu.edu.cn>
2018-12-13 23:16:35 +0800

    finish helloworld

--- code/helloworld.cu
+++ code/helloworld.cu
@@ -1,1 +1,10 @@
 #include <stdio.h>
+__global__ void helloCUDA(float f) {
+    printf(("Hello thread %d, f=%f\n", threadIdx.x, f);
+}
+int main() {
+    dim3 grid(2, 4);
+    dim3 block(8, 16);
+    helloCUDA<<<grid, block>>>(1.23456f);
+    return 0;
+}
